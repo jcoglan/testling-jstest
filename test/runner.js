69defd74d4af11ec0e2d7d7efd752709bc910124
James Coglan <jcoglan@gmail.com>
2013-02-10 19:21:38 +0000

    Detect ROOT in test/runner.js.

--- test/runner.js
+++ test/runner.js
@@ -1,5 +1,5 @@
 JS.packages(function() { with(this) {
-  var ROOT = JS.ENV.ROOT || '.'
+  var ROOT = (typeof window === 'undefined') ? '.' : '..'
 
   autoload(/^(.*)Spec$/, {from: ROOT + '/test/specs', require: '$1'})
   autoload(/^.*$/, {from: ROOT + '/lib'})
